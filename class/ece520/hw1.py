0bd9f84027af2138673cef1dc7cdd30fc606692f
Sean Bruno <sbruno1@unm.edu>
2024-01-28 09:57:10 -0700

    Wrap code to 80 columns

--- class/ece520/hw1.py
+++ class/ece520/hw1.py
@@ -7,10 +7,14 @@ data = []
 wafer_diameter_cm = (8 * 2.54)
 
 for diesize in range(1,6):
-    wasted_dies = math.floor((math.pi * wafer_diameter_cm) / math.sqrt(2 * (diesize)))
-    total_dies = math.floor((math.pi * (wafer_diameter_cm / 2) ** 2) / (diesize))
-    percentage_wasted_area = math.floor((wasted_dies / total_dies) * 100)
-    print ("diesize(",diesize,") - wasted(",wasted_dies,") - total_dies(",total_dies,") - percentage_wasted(",percentage_wasted_area,")")
+    wasted_dies = math.floor(
+            (math.pi * wafer_diameter_cm) / math.sqrt(2 * (diesize)))
+    total_dies = math.floor(
+            (math.pi * (wafer_diameter_cm / 2) ** 2) / (diesize))
+    percentage_wasted_area = math.floor(
+            (wasted_dies / total_dies) * 100)
+    print ("diesize(",diesize,") - wasted(",wasted_dies,") - total_dies(",
+            total_dies,") - percentage_wasted(",percentage_wasted_area,")")
     data.insert(diesize, percentage_wasted_area)
 
 plot.plot(range(1,6), data)
