5c93612e197b168ed4207ca9cc97af18ffa816bf
Sean Bruno <sbruno1@unm.edu>
2024-01-28 09:47:21 -0700

    Finish graph for hw1_prob1

--- class/ece520/hw1.py
+++ class/ece520/hw1.py
@@ -4,13 +4,13 @@ import math
 import matplotlib.pyplot as plot
 
 data = []
-wafer_diameter_inches = 8
+wafer_diameter_cm = (8 * 2.54)
 
 for diesize in range(1,6):
-    print ("checking diesize: ", diesize + 1)
-    wasted_dies = (math.pi * wafer_diameter_inches) / math.sqrt(2 * (diesize + 1))
-    total_dies = (math.pi * (wafer_diameter_inches / 2) ** 2) / (diesize + 1)
-    percentage_wasted_area = (wasted_dies / total_dies) * 100
+    wasted_dies = math.floor((math.pi * wafer_diameter_cm) / math.sqrt(2 * (diesize)))
+    total_dies = math.floor((math.pi * (wafer_diameter_cm / 2) ** 2) / (diesize))
+    percentage_wasted_area = math.floor((wasted_dies / total_dies) * 100)
+    print ("diesize(",diesize,") - wasted(",wasted_dies,") - total_dies(",total_dies,") - percentage_wasted(",percentage_wasted_area,")")
     data.insert(diesize, percentage_wasted_area)
 
 plot.plot(range(1,6), data)
