cf453954b1dca209b1de52c688c2c6af97bb57ad
Sean Bruno <sbruno1@unm.edu>
2024-01-28 09:36:05 -0700

    Iterate correctly from 1 to 5 via range()

--- class/ece520/hw1.py
+++ class/ece520/hw1.py
@@ -4,12 +4,16 @@ import math
 import matplotlib.pyplot as plot
 
 data = []
+wafer_diameter_inches = 8
 
-for diesize in range(5):
+for diesize in range(1,6):
     print ("checking diesize: ", diesize + 1)
-    data.insert(diesize, (math.pi * 8) / math.sqrt(2 * (diesize + 1)))
+    wasted_dies = (math.pi * wafer_diameter_inches) / math.sqrt(2 * (diesize + 1))
+    total_dies = (math.pi * (wafer_diameter_inches / 2) ** 2) / (diesize + 1)
+    percentage_wasted_area = (wasted_dies / total_dies) * 100
+    data.insert(diesize, percentage_wasted_area)
 
-plot.plot(range(5), data)
+plot.plot(range(1,6), data)
 plot.title('ECE 520: HW 1: Plot 1')
 plot.ylabel('Percentage wasted area in CM^2')
 plot.xlabel('Die Size in CM^2')
